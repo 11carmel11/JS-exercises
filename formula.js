e4de0d72708b64565f7802b86eb759d8262315d9
11carmel11 <11carmel11@walla.com>
2021-08-31 19:56:59 +0300

    formula champions
    listing names and wins

---
+++ formula.js
@@ -0,0 +1,32 @@
+const formula1Champions = [
+    "Schumacher",
+    "Schumacher",
+    "Schumacher",
+    "Schumacher",
+    "Schumacher",
+    "Alonso",
+    "Alonso",
+    "Räikkönen",
+    "Hamilton",
+    "Button",
+    "Vettel",
+    "Vettel",
+    "Vettel",
+    "Vettel",
+    "Hamilton",
+    "Hamilton",
+    "Rosberg",
+    "Hamilton",
+    "Hamilton",
+    "Hamilton",
+    "Hamilton"
+]
+let winners = {}
+
+function countWin(driver) {
+    if (winners.hasOwnProperty(driver)) {
+        winners[driver]++;
+    }
+    else winners[driver] = 1;
+}
+formula1Champions.forEach(countWin)
